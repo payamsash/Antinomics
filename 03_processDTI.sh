7d8c520b161df559f450d9af047c082b705dde51
payam <payam.sadeghi74@gmail.com>
2025-09-24 11:23:50 +0200

    tracks increased to 2 million

--- 03_processDTI.sh
+++ 03_processDTI.sh
@@ -105,11 +105,11 @@ compute_fixel_fixel_connectome () {
 
     ### Perform whole-brain fibre tractography on the FOD template
     cd ../diffusion_template
-    tckgen -angle 22.5 -maxlen 250 -minlen 10 -power 1.0 wmfod_template.mif -seed_image template_mask.mif -mask template_mask.mif -select 10000000 -cutoff 0.10 tracks_10_million.tck
-    tcksift tracks_10_million.tck wmfod_template.mif tracks_1_million_sift.tck -term_number 1000000
+    tckgen -angle 22.5 -maxlen 250 -minlen 10 -power 1.0 wmfod_template.mif -seed_image template_mask.mif -mask template_mask.mif -select 20000000 -cutoff 0.10 tracks_20_million.tck
+    tcksift tracks_20_million.tck wmfod_template.mif tracks_2_million_sift.tck -term_number 2000000
 
     ### Generate fixel-fixel connectivity matrix
-    fixelconnectivity fixel_mask/ tracks_1_million_sift.tck matrix/
+    fixelconnectivity fixel_mask/ tracks_2_million_sift.tck matrix/
     fixelfilter fd smooth fd_smooth -matrix matrix/
     fixelfilter log_fc smooth log_fc_smooth -matrix matrix/
     fixelfilter fdc smooth fdc_smooth -matrix matrix/
